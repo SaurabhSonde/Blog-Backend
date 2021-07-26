e0bc0064866c8658f3a9740a17f6b122c55ff1ff
SaurabhSonde <saurabhsonde111@gmail.com>
2021-07-26 10:52:33 +0530

    user controller created

---
+++ controllers/user.js
@@ -0,0 +1,37 @@
+const { findByIdAndUpdate } = require("../models/user");
+const User = require("../models/user");
+
+exports.getUserById = (req, res, next, id) => {
+  User.findById(id).exec((err, user) => {
+    if (err || !user) {
+      return res.status(400).json({
+        error: "User not found",
+      });
+    }
+
+    req.profile = user;
+    next();
+  });
+};
+
+exports.getUser = (req, res) => {
+  req.profile.password = undefined;
+  return res.json(req.profile);
+};
+
+exports.updateUser = (req, res) => {
+  User.findByIdAndUpdate(
+    { _id: req.profile._id },
+    { $set: req.body },
+    { new: true, useFindAndModify: false },
+    (err, user) => {
+      if (err) {
+        return res.status(400).json({
+          error: "You are not authorized to update user",
+        });
+      }
+      user.password = undefined;
+      res.json(user);
+    }
+  );
+};
