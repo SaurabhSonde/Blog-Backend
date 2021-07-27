92005105641371724fec563fc5f3101b85527ff9
SaurabhSonde <saurabhsonde111@gmail.com>
2021-07-27 14:22:42 +0530

    added role to user

--- models/user.js
+++ models/user.js
@@ -1,25 +1,32 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    maxLength: 30,
-    required: true,
-    trim: true,
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      maxLength: 30,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    photo: {
+      type: Buffer,
+      contentType: String,
+    },
+    role: {
+      type: Number,
+      default: 0,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  photo: {
-    type: Buffer,
-    contentType: String,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("User", userSchema);
