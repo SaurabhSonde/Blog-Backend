1c96ce18982bf23a1200a86bdda23cdc9599378c
SaurabhSonde <saurabhsonde111@gmail.com>
2021-07-27 17:57:20 +0530

    typo fixed

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { check, validationResult } = require("express-validator");
+const { check } = require("express-validator");
 const { signup, signin, signout } = require("../controllers/auth");
 
 router.post(
