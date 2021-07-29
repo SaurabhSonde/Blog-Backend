600fe567e586b0ae232a5c194aa6313dfc448f6f
SaurabhSonde <saurabhsonde111@gmail.com>
2021-07-29 20:54:02 +0530

    typo fixed

--- routes/auth.js
+++ routes/auth.js
@@ -9,7 +9,7 @@ router.post(
     check("name", "name should be at least 3 char").isLength({ min: 3 }),
     check("email", "email is required").isEmail(),
     check("password", "password should be at least 5 char").isLength({
-      min: 3,
+      min: 5,
     }),
   ],
   signup
