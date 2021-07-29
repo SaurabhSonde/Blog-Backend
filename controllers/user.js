1a6d6e2c421a4337bc1e15d71052c4abad322873
SaurabhSonde <saurabhsonde111@gmail.com>
2021-07-29 22:37:55 +0530

    added user updation controller

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,8 @@
 const User = require("../models/user");
+const bcrypt = require("bcrypt");
+const formidable = require("formidable");
+const _ = require("lodash");
+const fs = require("fs");
 
 //get user by id
 exports.getUserById = (req, res, next, id) => {
@@ -19,20 +23,55 @@ exports.getUser = (req, res) => {
   return res.json(req.profile);
 };
 
-// TODO: Photo updation remain
 exports.updateUser = (req, res) => {
-  User.findByIdAndUpdate(
-    { _id: req.profile._id },
-    { $set: req.body },
-    { new: true, useFindAndModify: false },
-    (err, user) => {
+  let form = new formidable.IncomingForm();
+  form.keepExtensions = true;
+
+  form.parse(req, (err, fields, file) => {
+    if (err) {
+      return res.status(400).json({
+        error: "Problem in image",
+      });
+    }
+
+    const { name, email, password, profile_pic } = fields;
+    if (file.profile_pic.size == 0) {
+      return res.status(400).json({
+        error: "Please include image",
+      });
+    }
+
+    if (!name || !password || !email || profile_pic) {
+      return res.status(400).json({
+        error: "Please include all fields",
+      });
+    }
+
+    let user = fields;
+    user = _.extend(user, fields);
+    //handle file here
+    if (file.profile_pic) {
+      if (file.profile_pic.size > 5000000) {
+        return res.status(400).json({
+          error: "File size too big!",
+        });
+      }
+      user.profile_pic = fs.readFileSync(file.profile_pic.path);
+      user.profile_pic.contentType = file.profile_pic.type;
+    }
+    User.user = user;
+    User.findByIdAndUpdate(
+      { _id: req.profile._id },
+      { $set: fields },
+      { new: true, useFindAndModify: false }
+    ).exec((err, user) => {
       if (err) {
         return res.status(400).json({
           error: "You are not authorized to update user",
         });
       }
-      user.password = undefined;
+
       res.json(user);
-    }
-  );
+    });
+  });
 };
