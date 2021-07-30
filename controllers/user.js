469dc326d53f00e12a08145ab225229b807f0284
SaurabhSonde <saurabhsonde111@gmail.com>
2021-07-30 11:54:38 +0530

    password updation added

--- controllers/user.js
+++ controllers/user.js
@@ -3,6 +3,7 @@ const bcrypt = require("bcrypt");
 const formidable = require("formidable");
 const _ = require("lodash");
 const fs = require("fs");
+const { profile } = require("console");
 
 //get user by id
 exports.getUserById = (req, res, next, id) => {
@@ -24,24 +25,35 @@ exports.getUser = (req, res) => {
 };
 
 exports.updateUser = (req, res) => {
+  //restricting user who not admin to update his profile
+  if (req.profile.role === 0) {
+    return res.status(403).json({
+      error:
+        "Access Denied,You are an unauthorized user you can't update your profile.",
+    });
+  }
+
   let form = new formidable.IncomingForm();
   form.keepExtensions = true;
 
-  form.parse(req, (err, fields, file) => {
+  form.parse(req, async (err, fields, file) => {
     if (err) {
       return res.status(400).json({
         error: "Problem in image",
       });
     }
 
-    const { name, email, password, profile_pic } = fields;
-    if (file.profile_pic.size == 0) {
-      return res.status(400).json({
-        error: "Please include image",
-      });
+    const { name, email, password, profile_pic, role } = fields;
+
+    if (profile_pic) {
+      if (file.profile_pic.size == 0) {
+        return res.status(400).json({
+          error: "Please include image",
+        });
+      }
     }
 
-    if (!name || !password || !email || profile_pic) {
+    if (!name || !password || !email || !profile_pic) {
       return res.status(400).json({
         error: "Please include all fields",
       });
@@ -49,6 +61,7 @@ exports.updateUser = (req, res) => {
 
     let user = fields;
     user = _.extend(user, fields);
+
     //handle file here
     if (file.profile_pic) {
       if (file.profile_pic.size > 5000000) {
@@ -59,6 +72,11 @@ exports.updateUser = (req, res) => {
       user.profile_pic = fs.readFileSync(file.profile_pic.path);
       user.profile_pic.contentType = file.profile_pic.type;
     }
+
+    //generating salt to hash password
+    const salt = await bcrypt.genSalt(10);
+    //hashing user password
+    user.password = await bcrypt.hash(user.password, salt);
     User.user = user;
     User.findByIdAndUpdate(
       { _id: req.profile._id },
